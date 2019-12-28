9f3689ae6d531b7858a280da34471aab6ef70443
추헌오 <chooheonoh@gmail.com>
2019-12-28 16:17:50 +0900

    AvalonDock 추가

---
+++ Nabi_Components/Settings/SettingsModel/Models/XML/Converters/IAlternativeDataTypeHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SettingsModel.Models.XML.Converters
+{
+	internal interface IAlternativeDataTypeHandler
+	{
+		Type SourceDataType { get; }
+		Type TargetDataType { get; }
+
+		object Convert(object objectInput);
+		object ConvertBack(object objectEncryptedData);
+	}
+}
